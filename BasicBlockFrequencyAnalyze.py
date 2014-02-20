dcef0686c7d7e34cdbe12f358db5340ab2f1254d
Lucian Cojocar <cojocar@gmail.com>
2014-02-20 17:33:10 +0100

    basic_blocks: count how many times a bb was executed

---
+++ BasicBlockFrequencyAnalyze.py
@@ -0,0 +1,47 @@
+from TraceEntries import *
+from BasicBlocks import *
+import sys
+
+def get_bb_frequency(trace_file, basic_blocks):
+    # get most frequent bbs (i.e. the bbs that were executed most often
+    # 30s on 400MB ExecTrace.dat
+    all_bbs = []
+    exec_freq = {}
+    for bb in basic_blocks.get_basic_blocks():
+        all_bbs += [bb]
+        exec_freq[bb[0]] = 0
+
+    # the number of basic block is pretty small (usually)
+    # sort basic block by addresses
+    #all_bbs.sort(key=lambda t:t[0])
+    print("we have %d basic blocks" % (len(all_bbs)))
+
+    total = 1
+    all_accesses = []
+    for h, e in trace_file.generate_elements():
+        if e.__class__ == ExecutionTraceMemory:
+            try:
+                c = exec_freq[e._data['pc']]
+                exec_freq[e._data['pc']] = c+1
+            except:
+                pass
+            total += 1
+    bb_and_freq = []
+    for c in range(len(all_bbs)):
+        bb_and_freq += [(all_bbs[c][0], exec_freq[all_bbs[c][0]])]
+    #bb_and_freq = [for c in range(len(all_bbs)) (all_bbs[c], exec_freq[all_bbs[c]])]
+    bb_and_freq.sort(key=lambda t:-t[1])
+
+    return bb_and_freq[:20]
+
+    # count how many times the PC was in a certain bb
+def main(trace_file, basic_blocks):
+    bb_and_freq = get_bb_frequency(trace_file, basic_blocks)
+    print("got most frequent bbs")
+
+    for bb in bb_and_freq:
+        print("Basic block @0x%08x was executed 0x%08x times" 
+                % (bb[0], bb[1]))
+
+if __name__ == "__main__":
+    main(TraceFile(sys.argv[1]), BasicBlocks(sys.argv[2]))
