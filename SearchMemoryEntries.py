165e17be79095d4f15b88d62bee3c6646ca9e8a6
Jonas Zaddach <zaddach@eurecom.fr>
2014-04-02 13:54:47 +0200

    Added range definitions via command line switches to SearchMemoryEntries.py

--- SearchMemoryEntries.py
+++ SearchMemoryEntries.py
@@ -1,28 +1,45 @@
-import sys
+import argparse
+from ast import literal_eval
 from TraceEntries import *
 
 # rtc-timer, integrator_pit, serial
-ranges=[0x15000000, 0x13000000, 0x16000000]
-#ranges=[0x00000000]
-#page=   0x1000
-page=   0x10
+DEFAULT_RANGES = [(0x15000000, 0x10), (0x13000000, 0x10), (0x16000000, 0x10)]
 
 def touches_one(addr, base, size):
     return (addr < (base + size)) and (addr >= base)
 
-def touches_any(addr, my_ranges=ranges, my_size=page):
+def touches_any(addr, my_ranges=DEFAULT_RANGES):
     for b in my_ranges:
-        if touches_one(addr, b, my_size):
+        if touches_one(addr, b[0], b[1]):
             return True
     return False
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tracefile", metavar = "FILE", type = str, help = "Trace file")
+    parser.add_argument("-r", "--range", dest = "ranges", action = "append", type = str, help = "A memory range, e.g., 0x1000-0x2000")
+    
+    args = parser.parse_args()
+    
+    ranges = []
+    if args.ranges:
+        for r in args.ranges:
+            tmp = r.split("-")
+            assert(len(tmp) == 2) #start and end must be provided
+            start = literal_eval(tmp[0])
+            end = literal_eval(tmp[1])
+        
+            ranges.append((start, end - start))
+        
+    if not ranges:
+        ranges = DEFAULT_RANGES
 
-if __name__ == "__main__":
-    tf = TraceFile(sys.argv[1])
+    tf = TraceFile(args.tracefile)
 
     for h, p in tf.generate_elements():
         if h._data['type'] == ExecutionTraceType.TRACE_MEMORY:
             addr = p._data['address']
-            if touches_any(addr):
+            if touches_any(addr, ranges):
                 d = 'W' if ExecutionTraceMemory.EXECTRACE_MEM_WRITE & p._data['flags'] else 'R'
                 print("[%c]: @0x%0x [0x%08x] -> 0x%08x" % (d, p._data['pc'], addr, p._data['value']))
                 #print(hex(p._data['address'])),
@@ -31,3 +48,6 @@ if __name__ == "__main__":
         elif h._data['type'] == ExecutionTraceType.TRACE_INSTR_START:
             # do nothing, we don't care about this entry
             pass
+
+if __name__ == "__main__":
+    main()
